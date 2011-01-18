6d88c6a19b73e870126fbab995ed1842cf5dd97c
Pieter Martin <pieter.martin@gmail.com>
2011-01-18 21:26:00 +0200

    added export data support

--- net.sf.nakeduml.util/src/main/java/net/sf/nakeduml/util/DataGeneratorProperty.java
+++ net.sf.nakeduml.util/src/main/java/net/sf/nakeduml/util/DataGeneratorProperty.java
@@ -1,21 +1,36 @@
 package net.sf.nakeduml.util;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import javax.annotation.PostConstruct;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.Log;
 
+@Name("dataGeneratorProperty")
+@Scope(ScopeType.APPLICATION)
+@AutoCreate
 public class DataGeneratorProperty {
 
 	private Properties properties;
+	private Properties exportProperties;
+	@Logger
+	private Log logger;
 
-	@PostConstruct
+	@Create
 	public void init() {
 		properties = new Properties();
+		exportProperties = new Properties();
 		InputStream in;
 		try {
 			in = Thread.currentThread().getContextClassLoader().getResourceAsStream("data.generation.properties");
@@ -54,14 +69,38 @@ public class DataGeneratorProperty {
 		}
 		return result;
 	}
-	
+
 	public List<Integer> getSizeListOfPropertiesThatStartsWith(String propertyStartWith) {
 		List<String> tmp = getPropertiesThatStartsWith(propertyStartWith);
-		List<Integer> result = new  ArrayList<Integer>(tmp.size());
+		List<Integer> result = new ArrayList<Integer>(tmp.size());
 		int i = 0;
-		for (@SuppressWarnings("unused") String string : tmp) {
+		for (@SuppressWarnings("unused")
+		String string : tmp) {
 			result.add(i++);
 		}
 		return result;
+	}
+
+	public void putExportProperty(Object key, Object value) {
+		if (value != null) {
+			if (value instanceof String && !((String)value).isEmpty()) {
+				exportProperties.put(key, value);
+			} else if (!(value instanceof String)) {
+				exportProperties.put(key, value.toString());
+			}
+		}
+	}
+
+	public String exportPropertiesToFile(File f) {
+		try {
+			exportProperties.store(new FileOutputStream(f), "comments go here");
+			return "success";
+		} catch (FileNotFoundException e) {
+			logger.error("could not export properties to file", e);
+			return "failure" + e.getMessage();
+		} catch (IOException e) {
+			logger.error("could not export properties to file", e);
+			return "failure" + e.getMessage();
+		}
 	}
 }
