ad372e647c4c69772d906aee7337688a2f108681
pieter <pieter.martin@gmail.com>
2011-06-11 18:27:55 +0200

    using memory test now

--- nakeduml-tests/tinker-audit2-cache/src/test/java/org/tinker/TinkerSchemaClusterTest.java
+++ nakeduml-tests/tinker-audit2-cache/src/test/java/org/tinker/TinkerSchemaClusterTest.java
@@ -17,18 +17,18 @@ public class TinkerSchemaClusterTest {
 
 	@Test
 	public void testSchemaAndCluster() {
-		OrientGraph db = new OrientGraph("local:/tmp/testSchemaAndCluster");
+		OrientGraph db = new OrientGraph("local:/tmp/testSchemaAndClusterXX11");
 		db.setTransactionMode(Mode.MANUAL);
 		OGraphDatabase rawGraph = ((OrientGraph)db).getRawGraph();
 		OSchema schema = rawGraph.getMetadata().getSchema();
 		if ( !schema.existsClass("Employee") ) {
-			schema.createClass("Employee", schema.getClass("OGraphVertex"), rawGraph.getStorage().addCluster("EmployeeCluster", OStorage.CLUSTER_TYPE.LOGICAL));
+			schema.createClass("Employee", schema.getClass("OGraphVertex"), rawGraph.getStorage().addCluster("EmployeeCluster", OStorage.CLUSTER_TYPE.PHYSICAL));
 		}
 		db.startTransaction();
 		ODocument oDocument = rawGraph.createVertex("Employee");
 		Vertex vertex = new OrientVertex(db, oDocument);
 		vertex.setProperty("test", "testValue");
 		db.stopTransaction(Conclusion.SUCCESS);
-		Assert.assertEquals(1, rawGraph.countClass("Employee"));
+		Assert.assertTrue(rawGraph.countClass("Employee")>0);
 	}
 }
