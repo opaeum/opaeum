64865a3592f0cc5a3460728bc404886307f1b434
ampie <ampieb@gmail.com>
2011-10-05 15:54:30 +0200

    'asdf' :q :

--- src/Renamer.java
+++ src/Renamer.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Renamer{
 	public static void main(String[] args) throws IOException,InterruptedException{
@@ -7,16 +9,29 @@ public class Renamer{
 		rename(dir);
 	}
 	public static void rename(File dir) throws InterruptedException,IOException{
+		Set<String> ignore = new HashSet<String>();
+		ignore.add("target");
+		ignore.add("opeum-runtime");
+		ignore.add("target");
+		ignore.add("target");
+		ignore.add("target");
 		for(File child:dir.listFiles()){
 			if(child.isDirectory()){
-				if(!child.getName().equals("target")){
+				if(!ignore.contains(child.getName())){
 					rename(child);
 				}
-			}else if(child.isFile()){
-				if(child.getName().equals("opeum")){
-					Runtime.getRuntime().exec("git mv opeum opaeum").waitFor();
-				}
+			}else{
+				replace(child, "opeum", "opaeum");
+				replace(child, "Opeum", "Opaeum");
+				replace(child, "Opium", "Opaeum");
+				replace(child, "opium", "opaeum");
 			}
+		}
+	}
+	public static void replace(File child,String s,String replacement) throws InterruptedException,IOException{
+		if(child.getName().contains(s)){
+			System.out.println("renaming " + child.getName());
+			Runtime.getRuntime().exec("git mv " + child.getName() + " " + child.getName().replaceAll(s, replacement)).waitFor();
 		}
 	}
 }
