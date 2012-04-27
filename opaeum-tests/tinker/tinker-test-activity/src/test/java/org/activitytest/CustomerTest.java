1e766bbb87abc2fdd1a2ca98527ce5ddeacf42f3
pieter <pieter.martin@gmail.com>
2012-04-27 18:39:30 +0200

    ert

--- opaeum-tests/tinker/tinker-test-activity/src/test/java/org/activitytest/CustomerTest.java
+++ opaeum-tests/tinker/tinker-test-activity/src/test/java/org/activitytest/CustomerTest.java
@@ -4,18 +4,12 @@ import org.junit.Test;
 
 public class CustomerTest implements CustomerTestContract {
 
-
-
 	@Test
 	public void testhomeAddressInitialValue() {
-		Root parent = new Root();
-		Customer object = new Customer(parent);
 	}
 	
 	@Test
 	public void testworkAddressInitialValue() {
-		Root parent = new Root();
-		Customer object = new Customer(parent);
 	}
 
 }
